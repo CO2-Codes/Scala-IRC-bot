f60f468d858b35ae3fbca238e9538c519f087f1a
CO2-Codes <>
2019-03-22 19:56:31 +0100

    Added shutdown from PM.

--- src/main/scala/codes/co2/ircbot/listeners/administration/AdminListener.scala
+++ src/main/scala/codes/co2/ircbot/listeners/administration/AdminListener.scala
@@ -1,7 +1,7 @@
 package codes.co2.ircbot.listeners.administration
 
 import org.pircbotx.PircBotX
-import org.pircbotx.hooks.events.{ConnectEvent, MessageEvent}
+import org.pircbotx.hooks.events.{ConnectEvent, MessageEvent, PrivateMessageEvent}
 import org.pircbotx.hooks.{Event, ListenerAdapter}
 
 class AdminListener(botAdmins: Seq[String], helpText: String) extends ListenerAdapter {
@@ -16,16 +16,25 @@ class AdminListener(botAdmins: Seq[String], helpText: String) extends ListenerAd
     getBot(event).send().mode(getBot(event).getNick, "+B")
   }
 
+  override def onPrivateMessage(event: PrivateMessageEvent): Unit = {
+    event.getMessage match {
+      case "!quit" if botAdmins.contains(event.getUser.getNick) && event.getUser.isVerified =>
+        shutdown(getBot(event))
+    }
+  }
+
   override def onMessage(event: MessageEvent): Unit = {
     event.getMessage match {
       case "!help" => event.getChannel.send().message(helpText)
-      case "!quit" if botAdmins.contains(event.getUser.getNick) && event.getUser.isVerified => {
-        getBot(event).stopBotReconnect()
-        getBot(event).sendIRC().quitServer("I don't wanna go...")
-        System.exit(0)
-      }
-
+      case "!quit" if botAdmins.contains(event.getUser.getNick) && event.getUser.isVerified =>
+        shutdown(getBot(event))
     }
+  }
+
+  private def shutdown(bot: PircBotX): Unit = {
+    bot.stopBotReconnect()
+    bot.sendIRC().quitServer("I don't wanna go...")
+    System.exit(0)
   }
 
 }
